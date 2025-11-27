1461838fe8e7bc4c7cb5d605ec4c86c8da60d8d6
daffalahidayat <118967827+daff11@users.noreply.github.com>
2025-11-27 22:19:53 +0700

    tes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,12 +5,10 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 })
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
 
 module.exports = pool;
